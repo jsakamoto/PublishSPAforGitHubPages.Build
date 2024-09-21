6512177439f63cb271a887e31ca4c4ae8553afcb
jsakamoto <junichi.jp@gmail.com>
2024-09-21 17:25:42 +0900

    Organize the unit test project

--- PublishSPAforGitHubPages.Build.Test/PublishTest.cs
+++ PublishSPAforGitHubPages.Build.Test/PublishTest.cs
@@ -6,7 +6,6 @@ using AngleSharp.Html.Parser;
 using NUnit.Framework;
 using PublishSPAforGitHubPages.Build.Test.Internals;
 using PublishSPAforGitHubPages.Build.Test.Internals.Models;
-using static Toolbelt.Diagnostics.XProcess;
 using static Toolbelt.FileIO;
 
 namespace PublishSPAforGitHubPages.Build.Test;
@@ -16,16 +15,10 @@ public class PublishTest
 {
     private readonly HtmlParser _Parser = new();
 
-    public static readonly IEnumerable<object[]> TestPattern = [
-        ["HTTPS", ""],
-        ["HTTPS", "WorkDir"],
-        //new object[]{"HTTPS.git", ""},
-        //new object[]{"HTTPS.git", "WorkDir"},
-        //new object[]{"SSH", ""},
-        //new object[]{"SSH", "WorkDir"},
-        //new object[]{"SSH.git", ""},
-        //new object[]{"SSH.git", "WorkDir"},
-    ];
+    public static readonly IEnumerable<object[]> TestPattern =
+        from urlType in Enum.GetValues<OriginUrlType>()
+        from subDir in new[] { "", "WorkDir" }
+        select new object[] { urlType, subDir };
 
     private string GetBaseHref(string indexHtmlPath)
     {
@@ -36,106 +29,104 @@ public class PublishTest
     }
 
     [TestCaseSource(nameof(TestPattern))]
-    public async Task Publish_ProjectSite_Test(string protocol, string subDir)
+    public async Task Publish_ProjectSite_Test(OriginUrlType originUrlType, string subDir)
     {
-        using var workDir = WorkDir.SetupWorkDir(siteType: "Project", protocol);
-        var projectSrcDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "SampleApp");
-        var projectDir = Path.Combine(workDir, subDir);
-        XcopyDir(projectSrcDir, projectDir);
-
-        var publishedFilesDir = Path.Combine(projectDir, "public", "wwwroot");
+        // Given
+        using var workDir = WorkDir.SetupWorkDir(SiteType.ProjectSite, originUrlType, projectName: "SampleApp", projectLocation: subDir);
         var addedFiles = new[] { ".nojekyll", "404.html", ".gitattributes", "decode.min.js", "brotliloader.min.js" }
-            .ToDictionary(f => f, f => Path.Combine(publishedFilesDir, f));
-        var publishedIndexHtmlPath = Path.Combine(publishedFilesDir, "index.html");
-        var published404HtmlPath = addedFiles["404.html"];
+            .ToDictionary(f => f, f => Path.Combine(workDir.PublishDir, f));
+        var publishedIndexHtmlPath = Path.Combine(workDir.PublishDir, "index.html");
 
-        // At first, normal publishing doesn't contain any additional files.
-        (await Start("dotnet", "publish -c:Release -o:public", projectDir).WaitForExitAsync()).ExitCode.Is(0);
-        addedFiles.Values.Any(f => File.Exists(f)).IsFalse();
+        // When: At first, normal publishing... 
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public", workDir.ProjectDir);
+
+        // Then: doesn't contain any additional files,
+        addedFiles.ToList().ForEach(x => File.Exists(x.Value).IsFalse(message: $"{x.Key} shouldn't exist, but there is."));
 
         // and, the base URL is not rewrited.
         this.GetBaseHref(publishedIndexHtmlPath).Is("/foo/");
 
-        // Second, "GHPages" enabled publishing contain additional files for GitHub pages.
-        DeleteDir(Path.Combine(projectDir, "public"));
-        (await Start("dotnet", "publish -c:Release -o:public -p:GHPages=true", projectDir).WaitForExitAsync()).ExitCode.Is(0);
-        addedFiles.Values.All(f => File.Exists(f)).IsTrue();
+        // Given
+        DeleteDir(workDir.PublicDir);
+
+        // When: Second, "GHPages" enabled publishing...
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public -p:GHPages=true", workDir.ProjectDir);
+
+        // Then: contain additional files for GitHub pages.
+        addedFiles.ToList().ForEach(x => File.Exists(x.Value).IsTrue(message: $"{x.Key} should exist, but not."));
 
         // and, the base URL is rewrited to project sub path.
         this.GetBaseHref(publishedIndexHtmlPath).Is("/fizz.buzz/");
 
         // Validate that the "404.html" is a copy of the "index.html".
-        var indexHtmlBytes = File.ReadAllBytes(publishedIndexHtmlPath);
-        var _404HtmlBytes = File.ReadAllBytes(published404HtmlPath);
-        _404HtmlBytes.Is(indexHtmlBytes);
+        TestAssert.FilesAreEquals(
+            actualFilePath: addedFiles["404.html"],
+            expectedFilePath: publishedIndexHtmlPath);
     }
 
     [TestCaseSource(nameof(TestPattern))]
-    public async Task Publish_UserSite_Test(string protocol, string subDir)
+    public async Task Publish_UserSite_Test(OriginUrlType originUrlType, string subDir)
     {
-        using var workDir = WorkDir.SetupWorkDir(siteType: "User", protocol);
-        var projectSrcDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "SampleApp");
-        var projectDir = Path.Combine(workDir, subDir);
-        XcopyDir(projectSrcDir, projectDir);
-
-        var publishedFilesDir = Path.Combine(projectDir, "public", "wwwroot");
+        // Given
+        using var workDir = WorkDir.SetupWorkDir(SiteType.UserSite, originUrlType, projectName: "SampleApp", projectLocation: subDir);
         var addedFiles = new[] { ".nojekyll", "404.html", ".gitattributes", "decode.min.js", "brotliloader.min.js" }
-            .ToDictionary(f => f, f => Path.Combine(publishedFilesDir, f));
-        var publishedIndexHtmlPath = Path.Combine(publishedFilesDir, "index.html");
-        var published404HtmlPath = addedFiles["404.html"];
+            .ToDictionary(f => f, f => Path.Combine(workDir.PublishDir, f));
+        var publishedIndexHtmlPath = Path.Combine(workDir.PublishDir, "index.html");
 
-        // At first, normal publishing doesn't contain any additional files.
-        (await Start("dotnet", "publish -c:Release -o:public", projectDir).WaitForExitAsync()).ExitCode.Is(0);
-        addedFiles.Values.Any(f => File.Exists(f)).IsFalse();
+        // When: At first, normal publishing ...
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public", workDir.ProjectDir);
+
+        // Then: doesn't contain any additional files.
+        addedFiles.ToList().ForEach(x => File.Exists(x.Value).IsFalse(message: $"{x.Key} shouldn't exist, but there is."));
 
         // and, the base URL is not rewrited.
         this.GetBaseHref(publishedIndexHtmlPath).Is("/foo/");
 
-        // Second, "GHPages" enabled publishing contain additional files for GitHub pages.
-        DeleteDir(Path.Combine(projectDir, "public"));
-        (await Start("dotnet", "publish -c:Release -o:public -p:GHPages=true", projectDir).WaitForExitAsync()).ExitCode.Is(0);
-        addedFiles.Values.All(f => File.Exists(f)).IsTrue();
+        // Given
+        DeleteDir(workDir.PublicDir);
+
+        // When: Second, "GHPages" enabled publishing...
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public -p:GHPages=true", workDir.ProjectDir);
+
+        // Then: contain additional files for GitHub pages.
+        addedFiles.ToList().ForEach(x => File.Exists(x.Value).IsTrue(message: $"{x.Key} should exist, but not."));
 
         // and, the base URL is rewrited to root path.
         this.GetBaseHref(publishedIndexHtmlPath).Is("/");
 
         // Validate that the "404.html" is a copy of the "index.html".
-        var indexHtmlBytes = File.ReadAllBytes(publishedIndexHtmlPath);
-        var _404HtmlBytes = File.ReadAllBytes(Path.Combine(publishedFilesDir, "404.html"));
-        _404HtmlBytes.Is(indexHtmlBytes);
+        TestAssert.FilesAreEquals(
+            actualFilePath: addedFiles["404.html"],
+            expectedFilePath: publishedIndexHtmlPath);
     }
 
     [Test]
     public async Task Publish_DisableComprression_Test()
     {
-        using var workDir = WorkDir.SetupWorkDir(siteType: "Project", protocol: "HTTPS");
-        var projectSrcDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "SampleApp");
-        var projectDir = Path.Combine(workDir, "WorkDir");
-        XcopyDir(projectSrcDir, projectDir);
+        // Given
+        using var workDir = WorkDir.SetupWorkDir(SiteType.ProjectSite, OriginUrlType.HTTPS, projectName: "SampleApp", projectLocation: "WorkDir");
 
-        var dotnetCLI = Start("dotnet", "publish -c:Release -o:public -p:CompressionEnabled=false -p:GHPages=true", projectDir);
-        await dotnetCLI.WaitForExitAsync();
-        dotnetCLI.ExitCode.Is(0, message: dotnetCLI.Output);
+        // When
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public -p:CompressionEnabled=false -p:GHPages=true", workDir.ProjectDir);
 
-        var publishedFilesDir = Path.Combine(projectDir, "public", "wwwroot");
+        // Then...
 
-        var actualPublishedFiles = Directory.GetFiles(publishedFilesDir).OrderBy(name => name).ToArray();
-
-        // compression files are not exists.
+        // compression files are not exists. (Only added files are exists in publish dir.)
+        var actualPublishedFiles = Directory.GetFiles(workDir.PublishDir).OrderBy(name => name).ToArray();
         var expectedPublishedFiles = new[] { ".nojekyll", "404.html", ".gitattributes", "index.html", "favicon.ico", "manifest.json", "service-worker.js", "my-assets.js" }
-            .ToDictionary(name => name, name => Path.Combine(publishedFilesDir, name));
+            .ToDictionary(name => name, name => Path.Combine(workDir.PublishDir, name));
         actualPublishedFiles.Is(expectedPublishedFiles.Values.OrderBy(name => name));
 
         // Brotli loader was not injected.
-        var expectedIndexHtmlContents = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "StaticFiles", "Rewrited", "index - brotli loader is not injected.html"));
-        var actualIndexHtmlContents = File.ReadAllText(expectedPublishedFiles["index.html"]);
-        actualIndexHtmlContents.Is(expectedIndexHtmlContents);
+        TestAssert.FilesAreEquals(
+            actualFilePath: expectedPublishedFiles["index.html"],
+            expectedFilePath: Path.Combine(workDir.FixtureDir, "Expected", "index of PWA - brotli loader is not injected.html"));
 
+        // Verify the file hash in the service worker assets manifest.
         using var sha256 = SHA256.Create();
         var expectedIndexHtmlBytes = File.ReadAllBytes(expectedPublishedFiles["index.html"]);
         var hash = "sha256-" + Convert.ToBase64String(sha256.ComputeHash(expectedIndexHtmlBytes));
 
-        // Verify the file hash in the service worker assets manifest.
         var serviceWorkerAssetsJs = File.ReadAllText(expectedPublishedFiles["my-assets.js"]);
         serviceWorkerAssetsJs = Regex.Replace(serviceWorkerAssetsJs, @"^self\.assetsManifest\s*=\s*", "");
         serviceWorkerAssetsJs = Regex.Replace(serviceWorkerAssetsJs, ";\\s*$", "");
@@ -144,8 +135,7 @@ public class PublishTest
         assetsManifestFile.IsNotNull();
         assetsManifestFile.assets.IsNotNull();
         var assetManifestEntry = assetsManifestFile.assets.FirstOrDefault(a => a.url == "index.html");
-        assetManifestEntry.IsNotNull();
-        assetManifestEntry.hash.Is(hash);
+        assetManifestEntry.IsNotNull().hash.Is(hash);
 
         // Verify the assets manifest doesn't include compressed file path such as ".dll.br" or ".dll.bz".
         assetsManifestFile.assets.Any(a => a.url?.EndsWith(".br") == true || a.url?.EndsWith(".bz") == true).IsFalse();
@@ -154,55 +144,40 @@ public class PublishTest
     [Test]
     public async Task Publish_NonPWA_Test()
     {
-        using var workDir = WorkDir.SetupWorkDir(siteType: "Project", protocol: "HTTPS");
-        var projectSrcDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "SampleAppNonPWA");
-        var projectDir = Path.Combine(workDir, "WorkDir");
-        XcopyDir(projectSrcDir, projectDir);
+        // Given
+        using var workDir = WorkDir.SetupWorkDir(SiteType.ProjectSite, OriginUrlType.HTTPS, projectName: "SampleAppNonPWA", projectLocation: "WorkDir");
 
-        var dotnetCLI = Start("dotnet", "publish -c:Release -o:public -p:CompressionEnabled=false -p:GHPages=true", projectDir);
-        await dotnetCLI.WaitForExitAsync();
-        dotnetCLI.ExitCode.Is(0, message: dotnetCLI.Output);
+        // When & Then
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public -p:CompressionEnabled=false -p:GHPages=true", workDir.ProjectDir);
     }
 
     [Test]
     public async Task Publish_PWA_For_NonGHPages_Test()
     {
-        using var workDir = WorkDir.SetupWorkDir(siteType: "Project", protocol: "HTTPS");
-        var projectSrcDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "SampleApp");
-        var projectDir = Path.Combine(workDir, "WorkDir");
-        XcopyDir(projectSrcDir, projectDir);
+        // Given
+        using var workDir = WorkDir.SetupWorkDir(SiteType.ProjectSite, OriginUrlType.HTTPS, projectName: "SampleApp", projectLocation: "WorkDir");
 
-        var dotnetCLI = Start("dotnet", "publish -c:Release -o:public -p:GHPagesBase=\"/foo/\"", projectDir);
-        await dotnetCLI.WaitForExitAsync();
-        dotnetCLI.ExitCode.Is(0, message: dotnetCLI.Output);
+        // When
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public -p:GHPagesBase=\"/foo/\"", workDir.ProjectDir);
 
-        var actualIndexHtmlPath = Path.Combine(projectDir, "public", "wwwroot", "index.html");
-        var actualIndexHtmlContents = File.ReadAllLines(actualIndexHtmlPath);
-
-        var expectedIndexHtmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "StaticFiles", "Expected", "index of PWA - no changed.html");
-        var expectedIndexHtmlContents = File.ReadAllLines(expectedIndexHtmlPath);
-
-        actualIndexHtmlContents.Is(expectedIndexHtmlContents);
+        // Then
+        var actualIndexHtmlPath = Path.Combine(workDir.PublishDir, "index.html");
+        var expectedIndexHtmlPath = Path.Combine(workDir.FixtureDir, "Expected", "index of PWA - no changed.html");
+        TestAssert.FilesAreEquals(actualIndexHtmlPath, expectedIndexHtmlPath);
     }
 
     [Test]
     public async Task Publish_NonPWA_For_NonGHPages_Test()
     {
-        using var workDir = WorkDir.SetupWorkDir(siteType: "Project", protocol: "HTTPS");
-        var projectSrcDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "SampleAppNonPWA");
-        var projectDir = Path.Combine(workDir, "WorkDir");
-        XcopyDir(projectSrcDir, projectDir);
+        // Given
+        using var workDir = WorkDir.SetupWorkDir(SiteType.ProjectSite, OriginUrlType.HTTPS, projectName: "SampleAppNonPWA", projectLocation: "WorkDir");
 
-        var dotnetCLI = Start("dotnet", "publish -c:Release -o:public -p:GHPagesBase=\"/foo/\"", projectDir);
-        await dotnetCLI.WaitForExitAsync();
-        dotnetCLI.ExitCode.Is(0, message: dotnetCLI.Output);
+        // When
+        await TestAssert.RunAsync("dotnet", "publish -c:Release -o:public -p:GHPagesBase=\"/foo/\"", workDir.ProjectDir);
 
-        var actualIndexHtmlPath = Path.Combine(projectDir, "public", "wwwroot", "index.html");
-        var actualIndexHtmlContents = File.ReadAllLines(actualIndexHtmlPath);
-
-        var expectedIndexHtmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Fixtures", "StaticFiles", "Expected", "index of NonPWA - no changed.html");
-        var expectedIndexHtmlContents = File.ReadAllLines(expectedIndexHtmlPath);
-
-        actualIndexHtmlContents.Is(expectedIndexHtmlContents);
+        // Then
+        var actualIndexHtmlPath = Path.Combine(workDir.PublishDir, "index.html");
+        var expectedIndexHtmlPath = Path.Combine(workDir.FixtureDir, "Expected", "index of NonPWA - no changed.html");
+        TestAssert.FilesAreEquals(actualIndexHtmlPath, expectedIndexHtmlPath);
     }
 }
